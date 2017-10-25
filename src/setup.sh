8559ec4754ae3a64abbba013b803ad33f78c75ff
Edgar Khanzadian <edgar.khanzadyan@yandex.ru>
2017-10-25 23:30:38 +0400

    updated setup.sh for caffe2 commit 01827c153d

--- src/setup.sh
+++ src/setup.sh
@@ -1,11 +1,14 @@
-echo "[Step1] Installing dependencies"
-brew install automake libtool
+#echo "[Step1] Installing dependencies"
+#brew install automake libtool
 
-echo "[Step2] git cloning Caffe2"
+echo "[Step1] git cloning Caffe2"
 git clone --recursive https://github.com/caffe2/caffe2.git
 
-echo "[Step2 - b] checkout to a stable version tags/v0.7.0-163-gebc17cc8"
-cd caffe2 && git checkout tags/v0.7.0-163-gebc17cc8
+echo "[Step2] checkout to a stable version tags/v0.7.0-163-gebc17cc8"
+cd caffe2 && git reset --hard 01827c153d
+
+echo "[Step3] update submodules"
+git submodule update
 
 echo "[Step3] build ios"
 ./scripts/build_ios.sh
